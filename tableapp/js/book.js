54ff3d10c4ca01f550e9566e75e2165e5a5622ee
soongcw <cwsoong@gmail.com>
2021-04-14 10:07:53 +0800

    4141007

--- tableapp/js/book.js
+++ tableapp/js/book.js
@@ -13,7 +13,7 @@ bookNowBtn.addEventListener("click", function () {
 });
 
 function BookNow(userName, userEmail, userPax, userRemarks) {
-    let url = 'https://api.sheety.co/b9b23bacbce0fa05289abc34d8cf52e6/tableapp/bookingitems';
+    let url ='https://api.sheety.co/d8b93f5ba4445dcf7fd47c3522d46056/tableapp/bookingitem';
     let body = {
         bookingitem: {
             name: userName,
